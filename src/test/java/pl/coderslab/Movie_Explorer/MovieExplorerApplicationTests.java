1de469343fb0ecc9d2f3c55fc780674a304269e3
krzysiek07410 <80361133+krzysiek07410@users.noreply.github.com>
2023-07-11 13:36:09 +0200

    Initial commit

---
+++ src/test/java/pl/coderslab/Movie_Explorer/MovieExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.Movie_Explorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
